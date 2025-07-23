fbf8059b40c7a6c46f1dcbaa01fb7eda661daab9
ifo-aka <ifhamk3435@gmail.com>
2025-07-23 10:56:27 +0500

    Delete src/main/webapp directory

--- src/main/webapp/script/Toggle.js
+++
@@ -1,7 +0,0 @@
-console.log("hello world")
-const toggle = document.getElementById('theme-toggle');
-toggle.addEventListener('click', () => {
-  document.documentElement.dataset.theme =
-    document.documentElement.dataset.theme === 'dark' ? 'light' : 'dark';
-});
-
